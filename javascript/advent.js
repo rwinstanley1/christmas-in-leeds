48619f23cab25cad115a11bb0333c29ed24ae28f
rwinstanley1 <56362072+rwinstanley1@users.noreply.github.com>
2019-11-19 11:16:38 +0000

    advent JS edit
    adding javascript grid and css highlighting to the links

--- javascript/advent.js
+++ javascript/advent.js
@@ -1,12 +1,15 @@
-var calenderLinks = document.getElementsByClassName('link');
+var grid = document.querySelector('.grid');
 
-for(let i = 0; i < calenderLinks.length; i++) {
-  calenderLinks[i].addEventListener("mouseover", function() {
-    console.log("Mouseover index: " + i);
-    calenderLinks[i].style.color = "red";
-  })
-  calenderLinks[i].addEventListener("mouseout", function() {
-    console.log("Mouseout index: " + i);
-    calenderLinks[i].style.color = "white";
-  })
-}
+var pckry = new Packery( grid, {
+  itemSelector: '.grid-item',
+  stagger: 30,
+});
+
+grid.addEventListener( 'click', function( event ) {
+  // filter for grid-item clicks
+  if ( !event.target.classList.contains('grid-item') ) {
+    return;
+  }
+  event.target.classList.toggle('grid-item--large');
+  pckry.layout();
+});

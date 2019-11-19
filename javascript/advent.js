1c3ca112e97fb6aa700a87ab9df1b3e40053fde5
rwinstanley1 <56362072+rwinstanley1@users.noreply.github.com>
2019-11-19 14:54:40 +0000

    removing unneccesary JS code

--- javascript/advent.js
+++ javascript/advent.js
@@ -9,6 +9,4 @@ grid.addEventListener( 'click', function( event ) {
   if ( !event.target.classList.contains('grid-item') ) {
     return;
   }
-  event.target.classList.toggle('grid-item--large');
-  pckry.layout();
 });
